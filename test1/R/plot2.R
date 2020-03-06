414bd28f45d86716d03d190f68757b1db1bb5bea
Casatimartina <60252770+Casatimartina@users.noreply.github.com>
2020-03-06 11:42:03 +0100

    Delete plot2.R

--- test1/R/plot2.R
+++
@@ -1,9 +0,0 @@
-#'A bar chart
-#'The function creates a bar chart rapresenting the mother'sweight in pounds at last menstrual period
-#'
-#' @return
-#' @export
-plot2 <- function(){
- p <- ggplot2::qplot(x = lwt, data = birthwt, geom = "bar")
-  return(p)
-}
